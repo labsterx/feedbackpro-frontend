12d4d16e4b63c454ec4909a5165674731be10842
LabserX <labserx@gmail.com>
2016-05-08 13:10:45 +0800

    code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,78 +18,28 @@ import {RouterActive} from './router-active';
   providers: [ ],
   directives: [ RouterActive ],
   encapsulation: ViewEncapsulation.None,
-  styles: [
-    require('normalize.css'),
-    `html, body{
-      height: 100%;
-      background: #F4FAFA;
-    }
-    button.active{
-      background: #fff;
-      color: #009688;
-    }
-    button.active:hover{
-      color: #fff;
-    }
-    .fill{
-      flex: 1 1 auto;
-    }
-    .app-state{
-      margin: 15px;
-      flex: 1;
-    }
-    .home{
-      flex: 1;
-    }
-    md-content{
-      display: flex;
-      flex-direction: column;
-      height: 100%;
-    }
-    footer{
-      flex: 0 0 60px;
-      padding: 10px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      background: #fff;
-    }`
-  ],
-  template: `
-    <md-content>
-      <md-toolbar color="primary">
-          <span>{{ name }}</span>
-          <span class="fill"></span>
-          <button md-button router-active [routerLink]=" ['Index'] ">
-            Index
-          </button>
-          <button md-button router-active [routerLink]=" ['Home'] ">
-            Home
-          </button>
-          <button md-button router-active [routerLink]=" ['About'] ">
-            About
-          </button>
-      </md-toolbar>
-      
-      <md-progress-bar mode="indeterminate" color="primary" *ngIf="loading"></md-progress-bar>
-
-      <router-outlet></router-outlet>
-
-      <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
-
-      <footer>
-        <img [src]="angularclassLogo" width="6%">
-        WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a>
-      </footer>
-      </md-content>
-  `
+  templateUrl: 'app/app.component.html'
 })
 @RouteConfig([
-  { path: '/',      name: 'Index', component: Home, useAsDefault: true },
-  { path: '/home',  name: 'Home',  component: Home },
+  { 
+    path: '/',      
+    name: 'Index', 
+    component: Home, 
+    useAsDefault: true 
+  },
+  { 
+    path: '/home',  
+    name: 'Home',  
+    component: Home 
+  },
   // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
-  { path: '/about', name: 'About', loader: () => require('es6-promise!./about')('About') }
+  { 
+    path: '/about', 
+    name: 'About', 
+    loader: () => require('es6-promise!./about')('About') 
+  }
 ])
+
 export class App {
   angularclassLogo = 'assets/img/angularclass-avatar.png';
   loading = false;
